e700bb47031e9b6707c4bcf52b494ced37b802cb
Spaghetti without Water <105888772+spaghetti-ohne-wasser@users.noreply.github.com>
2022-09-11 21:23:23 +0200

    Create Hangman.cpp

---
+++ Hangman.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    short failed;
+    std::string word, out;
+    char input;
+
+    std::cout << "Hangman" << std::endl <<"Enter the word to be searched for: ";
+    std::cin >> word;
+    std::cout << "\033[2J\033[1;1H";
+
+    for (int8_t i = 0; i < word.length(); i++)
+        out.append("_");
+
+    while (out.find("_") != std::string::npos && failed < 10)
+    {
+        std::cout << "The following word is searched:" << out << ", you have " << 10 - failed << " more tries" << std::endl;
+        std::cin >> input;
+        if (word.find(input) == std::string::npos)
+            failed++;
+        else
+            for (int8_t i = 0; i < word.length(); i++)
+                if (word[i] == input)
+                    out[i] = input;
+    }
+
+    if (out.find("_") != std::string::npos)
+        std::cout << "You have lost" << std::endl;
+    else
+        std::cout << "You have won! With " << failed << " failed attempts" << std::endl;
+    return 0;
+}
