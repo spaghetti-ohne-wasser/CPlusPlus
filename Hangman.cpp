5e62b9c437a8a3f7fff470b55eabd829a9e1dac7
Spaghetti without Water <105888772+spaghetti-ohne-wasser@users.noreply.github.com>
2022-10-13 21:23:24 +0200

    Update Hangman.cpp

--- Hangman.cpp
+++ Hangman.cpp
@@ -1,41 +1,33 @@
 #include <iostream>
 #include <string>
 
-#define clear "\033[2J\033[1;1H"
-#define red "\033[0;31m"
-#define green "\033[0;32m"
-#define white "\033[0;37m"
-
 int main()
 {
     char input;
-    int8_t failed, tries;
+    uint32_t failed, tries;
     std::string word, out;
 
     tries = 15;
-    
-    std::cout << "Hangman" << std::endl << "Enter the word to be searched for: ";
-    std::cin >> word;
-    std::cout << clear;
+    word = "HELLO";
 
-    for (int8_t i; i < word.length(); i++)
+    for (uint32_t i = 0; i < word.size(); i++)
         out.push_back('_');
 
     while (out.find("_") != std::string::npos && failed < tries)
     {
-        std::cout << "The following word is searched:" << out << ", you have " << tries - (int) failed << " more tries" << std::endl;
+        std::cout << "The following word is searched:" << out << ", you have " << tries - failed << " more tries" << std::endl;
         std::cin >> input;
         if (word.find(input) == std::string::npos)
             failed++;
         else
-            for (int8_t i = 0; i < word.length(); i++)
-                if (word[i] == input)
-                    out[i] = input;
+            for (uint32_t at = 0; at < word.length(); at++)
+                if (word[at] == input)
+                    out[at] = input;
     }
     if (out.find("_") != std::string::npos)
-        std::cout << red << clear << word << " You have lost";
+        std::cout << word << " You have lost :[";
     else
-        std::cout << green << clear << word << " You have won!";
-    std::cout << white << std::endl;
+        std::cout << word << " You have won :]";
+    std::cout << std::endl;
     return 0;
-}
+}   
