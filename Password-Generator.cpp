2d8141f6ff574f893145ff81848c81e1fa6824f4
Spaghetti without Water <105888772+spaghetti-ohne-wasser@users.noreply.github.com>
2022-10-13 18:45:24 +0200

    Create Password-Generator.cpp

---
+++ Password-Generator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+
+std::string Generate_Password(const uint32_t seed, const uint32_t length, const std::string chars)
+{
+    std::string password;
+    srand(seed);
+    for (uint32_t _ = 0; _ < length; _++)
+        password.push_back(chars[rand() % chars.size()]);
+    return password;
+}
+
+int main()
+{
+    std::cout << Generate_Password(time(NULL), 10, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") << std::endl;
+    return 0;
+}
