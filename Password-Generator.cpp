6633e7ad3effcf942dd3634c7fd4ae18d1e030bc
Spaghetti without Water <105888772+spaghetti-ohne-wasser@users.noreply.github.com>
2022-10-15 16:08:26 +0200

    Update Password-Generator.cpp

--- Password-Generator.cpp
+++ Password-Generator.cpp
@@ -1,33 +1,18 @@
 #include <iostream>
 #include <string>
+#include <ctime>
+
+std::string Generate_Password(const uint32_t seed, const uint32_t length, const std::string chars)
+{
+    std::string password;
+    srand(seed);
+    for (uint32_t _ = 0; _ < length; _++)
+        password.push_back(chars[rand() % chars.size()]);
+    return password;
+}
 
 int main()
 {
-    char input;
-    uint32_t failed, tries;
-    std::string word, out;
-
-    tries = 15;
-    word = "HELLO";
-
-    for (uint32_t i = 0; i < word.size(); i++)
-        out.append("_");
-
-    while (out.find("_") != std::string::npos && failed < tries)
-    {
-        std::cout << "The following word is searched:" << out << ", you have " << tries - failed << " more tries" << std::endl;
-        std::cin >> input;
-        if (word.find(input) == std::string::npos)
-            failed++;
-        else
-            for (uint32_t at = 0; at < word.length(); at++)
-                if (word[at] == input)
-                    out[at] = input;
-    }
-    if (out.find("_") != std::string::npos)
-        std::cout << word << " You have lost :[";
-    else
-        std::cout << word << " You have won :]";
-    std::cout << std::endl;
+    std::cout << Generate_Password(time(NULL), 10, "0123456789!@#$%^&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") << std::endl;
     return 0;
 }
